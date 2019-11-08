9aa087c87ef8d95a0a491d8211f6e71e6d291a80
Klim <simolick@gmail.com>
2019-11-08 21:27:31 +0000

    This is my first iteration of the project

---
+++ wordCount-project/views.py
@@ -0,0 +1,29 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from collections import Counter
+import operator
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def count(request):
+    fulltext = request.GET['fulltext']
+
+    wordlist = fulltext.split()
+
+    wordDictionary = dict(Counter(wordlist))
+    sortedWords = sorted(wordDictionary.items(), key=operator.itemgetter(1), reverse=True)
+
+    """
+    for word in wordlist:
+        if word in wordDictionary:
+            wordDictionary[word] += 1
+        else:
+            wordDictionary[word] = 1
+    """
+
+    return render(request, 'count.html', {'fulltext': fulltext, 'count': len(wordlist), 'sortedWords':sortedWords})
+
